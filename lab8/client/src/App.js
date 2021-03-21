a128a5ba4eb1d13bce061351980c383f221c2f4d
ChrisJones100 <59655390+ChrisJones100@users.noreply.github.com>
2021-03-20 21:30:14 -0400

    lab

---
+++ lab8/client/src/App.js
@@ -0,0 +1,14 @@
+import "./App.css";
+import Add from "./components/get";
+import Search from "./components/put";
+
+function App() {
+  return (
+    <div className="App">
+      <Search />
+      <Add />
+    </div>
+  );
+}
+
+export default App;
